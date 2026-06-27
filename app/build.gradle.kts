d42ab03b1b6b6c32727cfc3c1c7e0f3b6a4254d9
AbdullhGaber <abdullh760@yahoo.com>
2026-06-27 13:37:06 +0300

    chore: untrack IDE and build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(project(":domain"))
     implementation(project(":data"))
+    implementation(project(":presentation"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
